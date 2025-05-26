d4c14990abd4c560027d165bccf1ee6f31ac7561
LK152 <wuluke0@gmail.com>
2025-05-27 00:48:59 +0800

    update

--- 114/java-project/client/src/index.tsx
+++ 114/java-project/client/src/index.tsx
@@ -6,6 +6,8 @@ import Map from './Map';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'leaflet/dist/leaflet.css'
 import 'leaflet-routing-machine/dist/leaflet-routing-machine.css';
+import Login from './Login';
+import Signup from './Signup';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
@@ -17,6 +19,8 @@ root.render(
 			<Routes>
 				<Route path='/' element={<App />} />
 				<Route path='/map' element={<Map />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/signup' element={<Signup />} />
 			</Routes>
 		</Router>
 	</React.StrictMode>
